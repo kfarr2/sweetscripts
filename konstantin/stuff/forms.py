ecf30bd61069b5d05e8efa62880ebd3d1e1034cf
Konstantin <kfarr2@pdx.edu>
2014-12-26 21:09:01 -0800

    just a bunch of useless junk. i think.

--- konstantin/stuff/forms.py
+++ konstantin/stuff/forms.py
@@ -1,25 +1,26 @@
 from django import forms
 from django.forms import DateTimeField, ChoiceField, CharField
+from .tasks import get_screenshot
 from .models import Project, ProjectFile
 from bootstrap3_datetime.widgets import DateTimePicker
 
 class ProjectForm(forms.ModelForm):
-	date_completed = DateTimeField(required=False, label="Date Completed", widget=DateTimePicker(options={"format": "YYYY-MM-DD", "pickTime": False}))
+    date_completed = DateTimeField(required=False, label="Date Completed", widget=DateTimePicker(options={"format": "YYYY-MM-DD", "pickTime": False}))
 
-	class Meta:
-		model = Project
-		fields = (
-			'name',
-			'description',
-			'date_completed',
-			'team',
-			'screenshot',
-			'site_url',	
-		)
+    class Meta:
+        model = Project
+        fields = (
+            'name',
+            'description',
+            'date_completed',
+            'team',
+            'site_url', 
+        )
 
-	def __init__(self, *args, **kwargs):
-		super(ProjectForm, self).__init__(*args, **kwargs)
-	
-	def save(self, *args, **kwargs):
-		to_return = super(ProjectForm, self).save(*args, **kwargs)
-		return to_return
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+    
+    def save(self, *args, **kwargs):
+        to_return = super(ProjectForm, self).save(*args, **kwargs)
+        get_screenshot(self.cleaned_data['site_url']) 
+        return to_return
--- konstantin/utils/__init__.py
+++ konstantin/utils/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import datetime
 import sys
-
+import time
 
 from django import forms
 from django.forms.fields import Field
@@ -29,50 +29,4 @@ def get_client_ip(request):
         ip = request.META.get('REMOTE_ADDR')
     return ip
 
-'''
-Old shit. Delete. Possibly.
 
-
-forms.Form.required_css_class = "required"
-forms.ModelForm.required_css_class = "required"
-Field.required_css_class = "required"
-
-add_to_builtins('django.contrib.staticfiles.templatetags.staticfiles')
-add_to_builtins('konstantin.utils.templatetags.sickform')
-
-# add some helpful methods to the formset
-class FormSetMixin(object):
-	def iter_with_empty_form_first(self):
-		"""
-		Iterates over the forms in this formset, but the first form yielded
-		is the empty one. This simplifies the logic in templates, since the
-		empty_form is no longer a special case
-		"""
-		yield self.empty_form
-		for form in iter(self):
-			yield form
-	
-	def clean(self):
-		"""
-		When cleaning, if the form is being deleted, any errors on it should be
-		ignored
-		"""
-		for form in self.forms:
-			# this form is being deleted, so overwrite the errors
-			if form.cleaned_data.get("DELETE"):
-				form._errors = ErrorDict()
-
-# add the FormSetMixin to the base FormSet classes
-class BaseFormSet(FormSetMixin, BaseFormSet): pass
-class BaseModelFormSet(FormSetMixin, BaseModelFormSet): pass
-
-
-def dictfetchall(cursor):
-	"""Returns all rows from a cursor as a dict"""
-	desc = cursor.description
-	return [
-		dict(zip([col[0] for col in desc], row))
-		for row in cursor.fetchall()
-	]
-
-'''
