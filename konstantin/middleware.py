1fd0987ea24740bcf71ad6c05702460be79c159f
konstantin <konstantin@iceking>
2014-12-30 02:41:06 -0800

    Add support for the arduino

---
+++ konstantin/middleware.py
@@ -0,0 +1,17 @@
+import serial
+import os, sys, re
+import subprocess
+import tempfile
+from django.shortcuts import render_to_response
+from django.db import connection
+from time import time
+from operator import add
+from django.http import HttpResponse
+
+
+class ArduinoMiddleware:
+    def process_response(self, request, response):
+        connected = False
+        siri = serial.Serial("/dev/ttyACM0", 9600)
+        siri.write(str(response.status_code).encode())
+        return HttpResponse(response)
